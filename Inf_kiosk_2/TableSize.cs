7e7f3102390a0317da05e1d5d42c47db4b7a2eac
Елена <elena-zhdanova224@rambler.ru>
2014-02-26 10:22:34 +0400

    Новый проект не содержит некомпилируемого XAML. Путь изменён на относительный.

---
+++ Inf_kiosk_2/TableSize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inf_kiosk_2
+{
+    class TableSize
+    {
+
+        public void NumberCell(int n, out int colCount, out int rowCount)
+        {
+            colCount = (int)Math.Floor(Math.Sqrt(n * 4d / 3d));
+            rowCount = (int)Math.Ceiling((double)n / colCount);
+            if (n != 3) return;
+            colCount = 1;
+            rowCount = 3;
+        }
+    }
+}
