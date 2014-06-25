831e9ed86cf6607fe77163f6ad8d86c219ba2904
Елена <elena-zhdanova224@rambler.ru>
2014-06-25 10:00:19 +0400

    киоск до изменений

---
+++ Inf_kiosk_2/StartWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Inf_kiosk_2
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
